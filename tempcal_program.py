2fa250f5c263484105ddc422fbfb9d77dfad78b2
wasp_iSpy <nseabasionyinyechukwu@gmail.com>
2019-03-17 15:28:50 -0700

    new python files added

---
+++ tempcal_program.py
@@ -0,0 +1,23 @@
+def convert_to_celsius(fahrenheit):
+    """ (number) -> float
+
+    Return the number of Celsius degrees equivalent to fahrenheit degrees
+
+    >>> convert_to_celsius(75)
+    23.88888888889
+    """
+    return (fahrenheit - 32.0) * 5.0 / 9.0
+
+
+def above_freezing(celsius):
+    """ (number) -> bool
+
+    Return True iff temeperature celsius degrees is above freezing.
+
+    >>> above_freezing(5.2)
+    True
+    >>> above_freezing(-2)
+    False
+    """
+
+    return celsius > 0
